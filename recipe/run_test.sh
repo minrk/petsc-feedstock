1b48c1640c4eb1d046fbd256ad6172507aef5a53
Min RK <benjaminrk@gmail.com>
2016-10-27 11:17:29 +0200

    re-enable make runex1
    it should pass on circleci after rerendering with conda-smithy PR 337

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,8 +4,4 @@ export PETSC_DIR=${PREFIX}
 cd "${SRC_DIR}/src/snes/examples/tests"
 make ex1
 
-# FIXME: runex1 causes early termination with success (?!) on Linux
-# preventing upload. Still a complete mystery.
-if [[ "$(uname)" == "Darwin" ]]; then
-    make runex1
-fi
+make runex1
